eadc65289acd5773f218d27fef1eb56f6f9ce7f7
Familia <Familia@DBORA>
2021-04-21 02:00:45 -0300

    fim da aula 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,29 @@
 import React from "react";
-import { Header } from "../components/Header";
 
-export default function Home() {
-  return <h1>Index</h1>;
+export default function Home(props) {
+  return (
+    <div>
+      <h1>TODOS OS EPISÓDIOS</h1>
+      {props.episodes.map((episode) => (
+
+        <p>{episode.title}</p>
+      ))}
+
+    </div>
+  );
+}
+
+
+//SSG - Static Site Generation (para carregar poucas vezes ao dia e não a todo momento)
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 1,
+  }
+
 }
